bdfd0f1fac6d2b1463820cc9e0023bbf05e304f5
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-03-13 20:42:10 +0800

    Order80 注册进单机版Eureka

--- cloud-consumer-order80/src/main/java/com/grady/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/grady/springcloud/OrderMain80.java
@@ -3,6 +3,7 @@ package com.grady.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author gradyjiang
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @description: TODO
  * @date 2020/3/13
  */
+@EnableEurekaClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class OrderMain80 {
 
