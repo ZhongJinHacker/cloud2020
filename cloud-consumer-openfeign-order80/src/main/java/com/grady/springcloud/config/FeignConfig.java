13ad18898dbfa49615a68d2d5a8d98b787ff3b08
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-03-14 09:11:08 +0800

    使用OpenFeign调用，日志打印配置

---
+++ cloud-consumer-openfeign-order80/src/main/java/com/grady/springcloud/config/FeignConfig.java
@@ -0,0 +1,27 @@
+package com.grady.springcloud.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author gradyjiang
+ * @title: FeignConfig
+ * @projectName cloud2020
+ * @description: TODO
+ * @date 2020/3/14
+ */
+@Configuration
+public class FeignConfig {
+
+    /**
+     * feignClient配置日志级别
+     *
+     * @return
+     */
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        // 请求和响应的头信息,请求和响应的正文及元数据
+        return Logger.Level.FULL;
+    }
+}
