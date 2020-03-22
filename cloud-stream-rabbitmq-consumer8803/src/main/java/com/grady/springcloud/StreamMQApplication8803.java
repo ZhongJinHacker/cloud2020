2f6d2f8638c6593cec242db6e358c66e69d107f8
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-03-22 18:23:26 +0800

    rabbitmq spring cloud stream 整合 消息消费者分组消费，分组后消息会持久化

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/grady/springcloud/StreamMQApplication8803.java
@@ -0,0 +1,21 @@
+package com.grady.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gradyjiang
+ * @title: StreamMQApplication8803
+ * @projectName cloud2020
+ * @description: TODO
+ * @date 2020/3/22
+ */
+
+@SpringBootApplication
+public class StreamMQApplication8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQApplication8803.class, args);
+    }
+
+}
